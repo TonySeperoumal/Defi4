42ee565dfe394dfeaa013bee31ca8e6e29755986
titadone <bloum93200@gmail.com>
2019-12-13 16:37:20 +0100

    update test

--- Defi4/test/testplaneownership.js
+++ Defi4/test/testplaneownership.js
@@ -1,22 +1,22 @@
 const {BN,ether} = require('@openzeppelin/test-helpers');
 const {expect} = require('chai');
-const ERC21 = artifacts.require('PlaneOwnership');
+const ERC721 = artifacts.require('PlaneOwnership');
 
 
-contract('ERC21', function(accounts) {
-      const _name = 'ERC21 Token'
+contract('ERC721', function(accounts) {
+      const _name = 'ERC721 Token'
       const owner = accounts[0];
       const recipient = accounts[1];
 
       beforeEach(async function() {
-        this.ERC21Instance = await ERC21.new({from: owner});
+        this.ERC721Instance = await ERC721.new({from: owner});
       });
 
       it("propriétaire de zéro avions", async function() {
-        expect(await this.ERC21Instance.balanceOf.call(owner)).to.be.bignumber.equal(new BN('0'));
+        expect(await this.ERC721Instance.balanceOf.call(owner)).to.be.bignumber.equal(new BN('0'));
       });
 
       it("", async function() {
-        expect(await this.ERC21Instance.ownerOf.call(tokenId)).to.be.bignumber.equal(new BN('0'));
+        expect(await this.ERC721Instance.ownerOf.call(tokenId)).to.be.bignumber.equal(new BN('0'));
       });
   })
