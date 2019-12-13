e687b641ef4463257ca9c21a1adef4f517de208e
TonySeperoumal <tony.seperoumal@gmail.com>
2019-12-13 16:06:57 +0100

    test

--- Defi4/test/testplaneownership.js
+++ Defi4/test/testplaneownership.js
@@ -1,21 +1,15 @@
-const {
-  BN,
-  ether
-} = require('@openzeppelin/test-helpers');
-const {
-  expect
-} = require('chai');
-const ERC21 = artifacts.require('ERC21');
+const {BN,ether} = require('@openzeppelin/test-helpers');
+const {expect} = require('chai');
+const ERC21 = artifacts.require('PlaneOwnership');
+
 
 contract('ERC21', function(accounts) {
-      const _name = 'ERC21 Token';
+      const _name = 'ERC21 Token'
       const owner = accounts[0];
       const recipient = accounts[1];
 
       beforeEach(async function() {
-        this.ERC21Instance = await ERC21.new({
-          from: owner
-        });
+        this.ERC21Instance = await ERC21.new({from: owner});
       });
 
       it("propriétaire de zéro avions", async function() {
@@ -23,7 +17,6 @@ contract('ERC21', function(accounts) {
       });
 
       it("", async function() {
-        expect(await this.ERC21Instance.ownerOf.call(tokenId)).to.be.bignumber.equal(new BN('0'))
+        expect(await this.ERC21Instance.ownerOf.call(tokenId)).to.be.bignumber.equal(new BN('0'));
       });
-
-    }
+  })
